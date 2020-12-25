ae88cac5053fa982e1d0b838828aea8900bf53c6
Swapnil Misal <swapnil.misal@haptik.ai>
2020-12-25 11:49:46 +0530

    binary-tree: added easy solution

---
+++ binary-tree/isSameTree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * Checkout the LeetCode problem description
+ * {@link https://leetcode.com/problems/same-tree/ LeetCode},
+ * {@link https://github.com/swapnilmisal/algorithms-data-structures-implementation github}
+ * @author Swapnil Misal <swapnil.misal@gmail.com>
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  if (!p && !q) return true;
+  if (!p || !q || p.val !== q.val) return false;
+  return true && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
