91cc8ffc8df5e4ccaf104254c7eefd3554a83fbb
Swapnil Misal <swapnil.misal@gmail.com>
2021-01-17 17:26:07 +0530

    1043.partition-array-for-maximum-sum: add solution

--- leetcode/1043.partition-array-for-maximum-sum.js
+++ leetcode/1043.partition-array-for-maximum-sum.js
@@ -10,29 +10,18 @@
  * @param {number} k
  * @return {number}
  */
-var maxSumAfterPartitioning = function (arr, k) {
+var maxSumAfterPartitioning = function (arr, l) {
   let memo = new Array(arr.length).fill(0);
   let maxSum = 0;
-  for (let index = 0; index < arr.length; index++) {
-    for (let outerIndex = 0; outerIndex < k; outerIndex++) {
-      let elementIndex = index - outerIndex;
-      for (let innerIndex = k - outerIndex; innerIndex > 0; innerIndex--) {
-        let logicIndex = k - innerIndex + 1;
-        if (elementIndex - logicIndex < -1) {
-          break;
-        }
-        let tempSum =
-          arr[elementIndex] * logicIndex +
-          (memo[elementIndex - logicIndex] || 0);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i, count = 1; j > i - l && j >= 0; j--, count++) {
+      for (let k = count; k <= l && i - k >= -1; k++) {
+        let tempSum = arr[j] * k + (memo[i - k] || 0);
         if (tempSum > maxSum) maxSum = tempSum;
       }
     }
-    memo[index] = maxSum;
+    memo[i] = maxSum;
   }
-  console.log(memo);
-
   return maxSum;
 };
 // @lc code=end
-
-console.log(maxSumAfterPartitioning([1,15,7], 3));
