992923d874cff8f1169e8b0a74e84e422ed82c1b
Swapnil Misal <swapnil.misal@gmail.com>
2021-01-19 09:47:12 +0530

    94.binary-tree-inorder-traversal: add solution

---
+++ leetcode/94.binary-tree-inorder-traversal.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=94 lang=javascript
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root, res = []) {
+  if (!root) return res;
+  inorderTraversal(root.left, res);
+  res.push(root.val);
+  inorderTraversal(root.right, res);
+  return res;
+};
+// @lc code=end
