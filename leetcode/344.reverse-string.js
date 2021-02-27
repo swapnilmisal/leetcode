25dc603a7f576c20594efcc1cacf278253f58c75
Swapnil Misal <swapnil.misal@gmail.com>
2021-02-27 18:50:34 +0530

    344.reverse-string: two pointer solution

--- leetcode/344.reverse-string.js
+++ leetcode/344.reverse-string.js
@@ -10,11 +10,25 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function (s) {
+  let i = 0,
+    j = s.length - 1;
+  // two pointer solution
+  while (i < j) {
+    const temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+    i++;
+    j--;
+  }
+  return s;
+
+  /* 
   for (let index = 0; index < ~~(s.length / 2); index++) {
     const temp = s[index];
     s[index] = s[s.length - index - 1];
     s[s.length - index - 1] = temp;
   }
   return s;
+*/
 };
 // @lc code=end
