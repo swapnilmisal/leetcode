75f28774c8b0301af8944c7d10e8f6c2b3a03071
Swapnil Misal <swapnil.misal@gmail.com>
2021-03-08 09:03:42 +0530

    561.array-partition-i.js

--- leetcode/561.array-partition-i.js
+++ leetcode/561.array-partition-i.js
@@ -10,6 +10,8 @@
  * @return {number}
  */
 var arrayPairSum = function (nums) {
+  // sorting can take nlogn space and nlogn time complexity so it's not very good approach
+  // The preferable approach here could be to use linear sort and the space of the sorted array;
   const sortedNum = nums.sort((a, b) => a - b);
   let count = 0;
   for (let i = 0; i < sortedNum.length; i += 2) count += sortedNum[i];
