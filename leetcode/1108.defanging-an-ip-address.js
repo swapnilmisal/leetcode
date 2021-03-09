fb69e4f836b6280efe1e3da9ec02c804f72ee4f2
Swapnil Misal <swapnil.misal@gmail.com>
2021-03-09 10:05:08 +0530

    1108.defanging-an-ip-address

---
+++ leetcode/1108.defanging-an-ip-address.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1108 lang=javascript
+ *
+ * [1108] Defanging an IP Address
+ */
+
+// @lc code=start
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function (address) {
+  return address.replace(/\./ig, "[.]");
+};
+// @lc code=end
