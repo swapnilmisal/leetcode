607ff5828263a48b991a466ac56450a491766fb0
Swapnil Misal <swapnil.misal@gmail.com>
2021-03-11 21:16:59 +0530

    1603.design-parking-system

---
+++ leetcode/1603.design-parking-system.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=1603 lang=javascript
+ *
+ * [1603] Design Parking System
+ */
+
+// @lc code=start
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function (big, medium, small) {
+  this[1] = big;
+  this[2] = medium;
+  this[3] = small;
+};
+
+/**
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function (carType) {
+  if (this[carType]) {
+    this[carType]--;
+    return true;
+  }
+  return false;
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
+// @lc code=end
