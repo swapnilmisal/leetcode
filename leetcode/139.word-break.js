0b862e3837d2ece3de134d7b60ad553aff8139b7
Swapnil Misal <swapnil.misal@gmail.com>
2021-04-04 10:47:51 +0530

    139.word-break

--- leetcode/139.word-break.js
+++ leetcode/139.word-break.js
@@ -12,11 +12,18 @@
  */
 var wordBreak = function (s, wordDict) {
   const wordSet = new Set(wordDict);
+  const memo = [];
   const wordBreakRec = (i) => {
+    if (memo[i] !== undefined) return memo[i];
     for (let j = i + 1; j <= s.length; j++) {
       if (wordSet.has(s.slice(i, j))) {
-        if (s.length === j) return true;
-        else return wordBreakRec(j);
+        if (s.length === j) {
+          memo[i] = true;
+          return true;
+        } else {
+          memo[i] = wordBreakRec(j);
+          if (memo[i]) return memo[i];
+        }
       }
     }
     return false;
