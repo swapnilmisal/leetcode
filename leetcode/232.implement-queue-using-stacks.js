d5e221efc7a779dd01d0894ca83449e69375c155
Swapnil Misal <swapnil.misal@gmail.com>
2021-04-29 09:47:53 +0530

    232.implement-queue-using-stacks

---
+++ leetcode/232.implement-queue-using-stacks.js
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=232 lang=javascript
+ *
+ * [232] Implement Queue using Stacks
+ */
+
+// @lc code=start
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function () {
+  this.stack1 = [];
+  this.stack2 = [];
+};
+
+/**
+ * Push element x to the back of queue.
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this.stack1.push(x);
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  if (this.stack2.length) return this.stack2.pop();
+  else while (this.stack1.length) this.stack2.push(this.stack1.pop());
+  return this.stack2.pop();
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  if (this.stack2.length) return this.stack2[this.stack2.length - 1];
+  return this.stack1[0];
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  if (this.stack1.length || this.stack2.length) return false;
+  return true;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+// @lc code=end
