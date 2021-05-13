a20a424c93339038324e766a84e2fb03660adb2b
Swapnil Misal <swapnil.misal@gmail.com>
2021-05-13 19:34:31 +0530

    724.find-pivot-index

--- leetcode/724.find-pivot-index.js
+++ leetcode/724.find-pivot-index.js
@@ -10,16 +10,22 @@
  * @return {number}
  */
 var pivotIndex = function (nums) {
-  const leftSum = nums.reduce((acc, curr, index) => {
-    acc.push(index ? acc[index - 1] + curr : curr);
-    return acc;
-  }, []);
-
-  const rightSum = nums.reduceRight((acc, curr, index) => {
-    acc[index] = index === nums.length - 1 ? curr : curr + acc[index + 1];
-    return acc;
-  }, []);
-
-  return leftSum.findIndex((curr, index) => curr === rightSum[index]);
+  const sumFunc = (acc, curr) => acc + curr;
+  const sum = nums.reduce(sumFunc);
+  for (let i = 0, prefixSum = 0; i < nums.length; prefixSum += nums[i++])
+    if (prefixSum + nums[i] === sum - prefixSum) return i;
+  return -1;
+  // another approach
+  //   const leftSum = nums.reduce((acc, curr, index) => {
+  // acc.push(index ? acc[index - 1] + curr : curr);
+  // return acc;
+  //   }, []);
+  //
+  //   const rightSum = nums.reduceRight((acc, curr, index) => {
+  // acc[index] = index === nums.length - 1 ? curr : curr + acc[index + 1];
+  // return acc;
+  //   }, []);
+  //
+  //   return leftSum.findIndex((curr, index) => curr === rightSum[index]);
 };
 // @lc code=end
