fe42e52b01b4bc7bd0fd65e10408e3e6f9bae322
Swapnil Misal <swapnil.misal@gmail.com>
2021-07-09 09:01:52 +0530

    56.merge-intervals

---
+++ leetcode/56.merge-intervals.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+  let lowerLimit = 10 ** 4;
+  let upperLimit = 0;
+  const response = [];
+  const map = new Map(intervals);
+  intervals.forEach(([start, end]) => {
+    if (map.has(start)) {
+      const currentEnd = map.get(start);
+      const maxEnd = currentEnd > end ? currentEnd : end;
+      map.set(start, maxEnd);
+    } else {
+      map.set(start, end);
+    }
+
+    lowerLimit = lowerLimit > start ? start : lowerLimit;
+    upperLimit = upperLimit < start ? start : upperLimit;
+  });
+
+  for (let i = lowerLimit; i <= upperLimit; ) {
+    if (map.has(i)) {
+      let start = i;
+      let end = map.get(i);
+
+      while (i <= end) {
+        const nextEnd = map.get(i) ? map.get(i) : 0;
+        end = nextEnd > end ? nextEnd : end;
+        i++;
+      }
+      response.push([start, end]);
+    } else i++;
+  }
+  return response;
+};
