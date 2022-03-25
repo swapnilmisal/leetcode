53d32cec1a20fe01bd297e36a9a9615475a91aad
Swapnil Misal <swapnil.misal@gmail.com>
2022-03-25 10:42:55 +0530

    322.coin-change: add different ways to solve the problem

--- leetcode/322.coin-change.js
+++ leetcode/322.coin-change.js
@@ -10,6 +10,7 @@
  * @param {number} amount
  * @return {number}
  */
+//DFS memoisation
 var coinChange = function (coins, amount) {
   if (amount === 0) return 0;
   const memo = new Map();
@@ -28,4 +29,43 @@ var coinChange = function (coins, amount) {
   };
   return recurse(amount);
 };
+
+// DP-Tabulation
+// Tabulate array of length amount.
+// iterate from amount to 0 with memoisation and minimisation.
+// return the result at 0;
+// var coinChange = function (coins, amount) {
+
+// }
+//BFS
+/*
+var coinChange = function (coins, amount) {
+  const map = new Map();
+  const queue = [amount];
+  map.set(amount, 0);
+
+  while (queue.length) {
+    const current = queue[0];
+    const change = map.get(current) + 1;
+
+    coins.forEach(coin => {
+      const node = current - coin;
+      if (node < 0) return;
+      if (map.has(node)) {
+        const nodeChage = map.get(node);
+        if (change < nodeChage) {
+          map.set(node, change);
+          queue.push(node);
+        }
+      } else {
+        map.set(node, change);
+        queue.push(node);
+      }
+    })
+    queue.shift();
+  }
+  if (map.has(0)) return map.get(0);
+  return -1;
+}
+*/
 // @lc code=end
